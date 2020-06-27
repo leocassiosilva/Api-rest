0d3fdc9a88dffcf626cf90b02aa8cabdd8bb7f04
leocassiosilva <leocassiosilva1234@gmail.com>
2020-06-27 07:43:26 -0300

    metodos delete e put

--- src/main/java/br/com/produtos/apirest/controller/ProdutoResource.java
+++ src/main/java/br/com/produtos/apirest/controller/ProdutoResource.java
@@ -3,9 +3,11 @@ package br.com.produtos.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +39,17 @@ public class ProdutoResource {
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
+	
+	/*Metodo para deletar um produto*/
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	/*Metodo para atualizar um produto*/
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
 }
